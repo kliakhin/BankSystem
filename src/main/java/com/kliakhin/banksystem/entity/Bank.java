3bb1b43800c8fe7b27d542ab8aa31ce39cff51d6
Evgen Kliakhin <kliakhin.e.a@gmail.com>
2016-03-17 11:55:33 +0200

    add entities

---
+++ src/main/java/com/kliakhin/banksystem/entity/Bank.java
@@ -0,0 +1,44 @@
+package com.kliakhin.banksystem.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by Евгений on 03/17/16.
+ */
+@Entity
+@Table(name = "bank")
+public class Bank {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "incerement", strategy = "increment")
+    @Column(name= "id", length = 6, nullable = false)
+    private long id;
+    @Column(name = "name")
+    private String name;
+
+    public Bank() {
+    }
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/kliakhin/banksystem/entity/enums/WorkerStatus.java
@@ -0,0 +1,8 @@
+package com.kliakhin.banksystem.entity.enums;
+
+/**
+ * Created by Евгений on 03/17/16.
+ */
+public enum WorkerStatus {
+    MANAGER;
+}
